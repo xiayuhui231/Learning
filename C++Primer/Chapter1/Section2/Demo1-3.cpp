1f79211404082b388d88f22b4ceb17941332b185
YxY <876285983@qq.com>
2021-01-24 15:07:59 +0800

    feat(C++Promer/Chapter1): add section1, section2 Demo

---
+++ C++Primer/Chapter1/Section2/Demo1-3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Wrold!" << std::endl;
+    return 0;
+}
---
+++ C++Primer/Chapter1/Section2/Demo1-5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "V1 * V2 = ";
+    std::cout << v1 * v2;
+    std::cout << std::endl;
+}
